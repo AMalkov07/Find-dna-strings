59f7a9b123b074697d402ba5728e0238aa398444
AMalkov07 <andrey.malkov07@gmail.com>
2024-09-30 23:07:23 -0500

    added self_search() function, meant for finding patterns within a single string

--- similar_strings.py
+++ similar_strings.py
@@ -1,8 +1,3 @@
-#dict = {"s1": "cat",
-     #"s2": "dog",
-     #"s3": "cow"}
-
-
 def read_fasta(file_path):
     fasta_dict = {}
     with open(file_path, 'r') as f:
@@ -26,6 +21,7 @@ file_path = r"C:\Users\Andrey\Desktop\momWork\2024\Find-dna-strings\6991_only_te
 
 dict = read_fasta(file_path)
 
+#class used for the creation of a custom variable type that will store all the info that we are interested in
 class customType:
     def __init__(self, input_s_li, input_s_ri, dict_str, key_index, is_reverse):
         self.input_s_li = input_s_li
@@ -42,6 +38,7 @@ input_s = "TGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGGGTGTGGTGTGTGTGG
 max_length = 60
 output = {}
 
+# main function that finds whether or not parts of out input_s string are contained inside of our dictionary strings as substrings
 def rec(input_s, offset):
     #print(f"running rec on {input_s}")
     if len(input_s) < 20:
@@ -57,6 +54,37 @@ def rec(input_s, offset):
                 return
 
 
-rec(input_s, 0)
-for key in output.keys():
-    print(f'{output[key]}')
+class self_search_type:
+    def __init__(self, n_subStr, subStr_start, matches_start_indexes):
+        self.n_subStr = n_subStr
+        self.subStr_start = subStr_start
+        self.matches_start_indexes = matches_start_indexes
+
+    def __repr__(self):
+        return f"substring length={self.n_subStr}, number of matches={len(self.matches_start_indexes)}\n"
+
+def self_search(input_s):
+    my_dict = {}
+    n = len(input_s)
+    min_length = 3
+    for i in range(0, len(input_s)//2):
+        for j in range(min_length, n-i):
+            subStr = input_s[i:i+j]
+            if subStr in my_dict:
+                continue
+            for k in range(i+1, n-j+1):
+                if input_s[k:k+j] == subStr:
+                    if subStr in my_dict:
+                        my_dict[subStr].matches_start_indexes.append(k)
+                    else:
+                        my_dict[subStr] = self_search_type(j, i, [k])
+    sorted_dict = sorted(my_dict.items(), key=lambda item: item[1].n_subStr, reverse=True)
+    print(sorted_dict)
+#rec(input_s, 0)
+#for key in output.keys():
+    #print(f'{output[key]}')
+
+
+testStr = "TGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGTGGGTGTTGTGTGGGTGTGGTGTGTGGGTGTGGGTGTGTGTGTGTGTGGGTGTGGTGTGGGTGTGGTTGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGTGGGTGTTGTGTGGGTGTGGGTGGTGTGTGGGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTTGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGTGGGTGTTGTGTGGGTGTGTGTGTGGTGTGTGTGGGTGTGTGGGTGTGGTGTGTGTGGGTGTGGTGTTGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGTGGGTGTTGGTGTGGTGTGTGGGTGTGTGGGTGTGGGTGTGGTGTGGATGTGGTGTGGGTGTGGTGTTGTGTGTGGTGTGTGGGTGTGGGTGTGGTGTGTGTGGGTGGGTGTGGTGTGTGTGGGTGT"
+self_search(testStr)
+
