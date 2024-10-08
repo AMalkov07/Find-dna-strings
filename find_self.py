18ea0c3e054d234d3e4eb378877e9fa94a292a5f
AMalkov07 <andrey.malkov07@gmail.com>
2024-10-08 01:19:19 -0500

    made the made the functions inside of the find_self.py file actually worrk

--- find_self.py
+++ find_self.py
@@ -1,17 +1,46 @@
+from collections import deque, defaultdict
+
 class self_search_type:
-    def __init__(self, n_subStr, matches_start_indexes):
+    def __init__(self, n_subStr, indexes):
         self.n_subStr = n_subStr
         #self.subStr_start = subStr_start
-        self.matches_start_indexes = matches_start_indexes
+        self.indexes = indexes
 
     def __repr__(self):
-        return f"substring length={self.n_subStr}, number of matches={len(self.matches_start_indexes)}\n"
+        return f"substring length={self.n_subStr}, number of matches={len(self.indexes)}\n"
 
-def expand_dict(my_dict):
+def expand_dict(input_s, my_dict):
+    input_s_n = len(input_s)
+    queue = deque()
     for key in my_dict.keys():
-        print("hi")
+        queue.append(key)
+    while queue:
+        tmp = queue.pop()
+        n_string = my_dict[tmp].n_subStr
+        indexes = my_dict[tmp].indexes
+        n_indexes = len(indexes)
+        counter = 1
+        s = ""
+        for index in indexes:
+            r_index = index + n_string
+            if r_index >= input_s_n:
+                continue
+            s = input_s[index:r_index+1]
+            if s not in my_dict:
+                my_dict[s] = self_search_type(len(s), [index])
+            else:
+                my_dict[s].indexes.append(index)
+                counter += 1
+        if counter == n_indexes:
+            del my_dict[s[:-1]]
+    print(my_dict)
+
+
+
+
 
 def self_search(input_s):
+    #my_dict = defaultdict(lambda: 0)
     my_dict = {}
     n = len(input_s)
     min_length = 2 
@@ -32,7 +61,9 @@ def self_search(input_s):
                 else:
                     my_dict[subStr] = self_search_type(j, [i,k])
 
+    #print(my_dict)
     print(my_dict)
+    expand_dict(input_s, my_dict)
     #sorted_dict = sorted(my_dict.items(), key=lambda item: item[1].n_subStr, reverse=True)
     #print(sorted_dict)
 
